e59ec04bd98da6cd2f7d8719afe54afb55b87e88
sonibrothers18 <soni18august2004@gmail.com>
2024-04-04 00:53:30 +0530

    Java

--- Java_Lab/b2.java
+++
@@ -1,5 +0,0 @@
-public class b2 {
-        public static void main(String[] args) {
-            
-        }
-}
--- Java_Lab/constructor.java
+++
@@ -1,8 +0,0 @@
-public class constructor{
-    constructor(){
-        System.out.println("Default constructor created");
-        public static void main(String[] args){
-            constructor ex1 = new constructor();
-        }
-    }
-}
